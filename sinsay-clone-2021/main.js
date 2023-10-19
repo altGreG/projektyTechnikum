2728e6af5fe5b97aed1626544efee09a8742e36a
altGreG <grzeg.bak.pl@gmail.com>
2023-10-19 20:21:38 +0200

    kara za pracowanie na złej gałęzi, naprawa struktury katalogowej gałęzi main

---
+++ sinsay-clone-2021/main.js
@@ -0,0 +1,127 @@
+// kod do zwykłego menu
+document.querySelector(".dla-niej").onmouseover = () => mouseOver(".dla-niej-block");
+		document.querySelector(".dla-niej").onmouseout = function() {mouseOut(".dla-niej-block")};
+		document.querySelector(".dla-niej-block").onmouseover = function() {mouseOver2(".dla-niej-block")};
+		document.querySelector(".dla-niej-block").onmouseout = function() {mouseOut2(".dla-niej-block")};
+
+		document.querySelector(".dla-niego").onmouseover = () => mouseOver(".dla-niego-block");
+		document.querySelector(".dla-niego").onmouseout = function() {mouseOut(".dla-niego-block")};
+		document.querySelector(".dla-niego-block").onmouseover = function() {mouseOver2(".dla-niego-block")};
+		document.querySelector(".dla-niego-block").onmouseout = function() {mouseOut2(".dla-niego-block")};
+
+		document.querySelector(".dla-dziecka").onmouseover = () => mouseOver(".dla-dziecka-block");
+		document.querySelector(".dla-dziecka").onmouseout = function() {mouseOut(".dla-dziecka-block")};
+		document.querySelector(".dla-dziecka-block").onmouseover = function() {mouseOver2(".dla-dziecka-block")};
+		document.querySelector(".dla-dziecka-block").onmouseout = function() {mouseOut2(".dla-dziecka-block")};
+
+		document.querySelector(".dla-niej-nowosci").onmouseover = () => mouseOver(".dla-niej-nowosci-block");
+		document.querySelector(".dla-niej-nowosci").onmouseout = function() {mouseOut(".dla-niej-nowosci-block")};
+		document.querySelector(".dla-niej-nowosci-block").onmouseover = function() {mouseOver2(".dla-niej-nowosci-block")};
+		document.querySelector(".dla-niej-nowosci-block").onmouseout = function() {mouseOut2(".dla-niej-nowosci-block")};
+
+		document.querySelector(".dla-niego-nowosci").onmouseover = () => mouseOver(".dla-niego-nowosci-block");
+		document.querySelector(".dla-niego-nowosci").onmouseout = function() {mouseOut(".dla-niego-nowosci-block")};
+		document.querySelector(".dla-niego-nowosci-block").onmouseover = function() {mouseOver2(".dla-niego-nowosci-block")};
+		document.querySelector(".dla-niego-nowosci-block").onmouseout = function() {mouseOut2(".dla-niego-nowosci-block")};
+
+		document.querySelector(".dla-dziecka-nowosci").onmouseover = () => mouseOver(".dla-dziecka-nowosci-block");
+		document.querySelector(".dla-dziecka-nowosci").onmouseout = function() {mouseOut(".dla-dziecka-nowosci-block")};
+		document.querySelector(".dla-dziecka-nowosci-block").onmouseover = function() {mouseOver2(".dla-dziecka-nowosci-block")};
+		document.querySelector(".dla-dziecka-nowosci-block").onmouseout = function() {mouseOut2(".dla-dziecka-nowosci-block")};
+
+		document.querySelector(".nowosci-dla-domu").onmouseover = () => mouseOver(".nowosci-dla-domu-block");
+		document.querySelector(".nowosci-dla-domu").onmouseout = function() {mouseOut(".nowosci-dla-domu-block")};
+		document.querySelector(".nowosci-dla-domu-block").onmouseover = function() {mouseOver2(".nowosci-dla-domu-block")};
+		document.querySelector(".nowosci-dla-domu-block").onmouseout = function() {mouseOut2(".nowosci-dla-domu-block")};
+
+		document.querySelector(".dla-dziewczynki").onmouseover = () => mouseOver(".dla-dziewczynki-block");
+		document.querySelector(".dla-dziewczynki").onmouseout = function() {mouseOut(".dla-dziewczynki-block")};
+		document.querySelector(".dla-dziewczynki-block").onmouseover = function() {mouseOver2(".dla-dziewczynki-block")};
+		document.querySelector(".dla-dziewczynki-block").onmouseout = function() {mouseOut2(".dla-dziewczynki-block")};
+
+		document.querySelector(".dla-chlopca").onmouseover = () => mouseOver(".dla-chlopca-block");
+		document.querySelector(".dla-chlopca").onmouseout = function() {mouseOut(".dla-chlopca-block")};
+		document.querySelector(".dla-chlopca-block").onmouseover = function() {mouseOver2(".dla-chlopca-block")};
+		document.querySelector(".dla-chlopca-block").onmouseout = function() {mouseOut2(".dla-chlopca-block")};
+
+		document.querySelector(".do-12-lat").onmouseover = () => mouseOver(".do-12-lat-block");
+		document.querySelector(".do-12-lat").onmouseout = function() {mouseOut(".do-12-lat-block")};
+		document.querySelector(".do-12-lat-block").onmouseover = function() {mouseOver2(".do-12-lat-block")};
+		document.querySelector(".do-12-lat-block").onmouseout = function() {mouseOut2(".do-12-lat-block")};
+
+		document.querySelector(".bedroom").onmouseover = () => mouseOver(".bedroom-block");
+		document.querySelector(".bedroom").onmouseout = function() {mouseOut(".bedroom-block")};
+		document.querySelector(".bedroom-block").onmouseover = function() {mouseOver2(".bedroom-block")};
+		document.querySelector(".bedroom-block").onmouseout = function() {mouseOut2(".bedroom-block")};
+
+		document.querySelector(".livingroom").onmouseover = () => mouseOver(".livingroom-block");
+		document.querySelector(".livingroom").onmouseout = function() {mouseOut(".livingroom-block")};
+		document.querySelector(".livingroom-block").onmouseover = function() {mouseOver2(".livingroom-block")};
+		document.querySelector(".livingroom-block").onmouseout = function() {mouseOut2(".livingroom-block")};
+
+		document.querySelector(".child-room").onmouseover = () => mouseOver(".child-room-block");
+		document.querySelector(".child-room").onmouseout = function() {mouseOut(".child-room-block")};
+		document.querySelector(".child-room-block").onmouseover = function() {mouseOver2(".child-room-block")};
+		document.querySelector(".child-room-block").onmouseout = function() {mouseOut2(".child-room-block")};
+
+		document.querySelector(".bathroom").onmouseover = () => mouseOver(".bathroom-block");
+		document.querySelector(".bathroom").onmouseout = function() {mouseOut(".bathroom-block")};
+		document.querySelector(".bathroom-block").onmouseover = function() {mouseOver2(".bathroom-block")};
+		document.querySelector(".bathroom-block").onmouseout = function() {mouseOut2(".bathroom-block")};
+		
+		document.querySelector(".make-up").onmouseover = () => mouseOver(".make-up-block");
+		document.querySelector(".make-up").onmouseout = function() {mouseOut(".make-up-block")};
+		document.querySelector(".make-up-block").onmouseover = function() {mouseOver2(".make-up-block")};
+		document.querySelector(".make-up-block").onmouseout = function() {mouseOut2(".make-up-block")};
+
+		document.querySelector(".care").onmouseover = () => mouseOver(".care-block");
+		document.querySelector(".care").onmouseout = function() {mouseOut(".care-block")};
+		document.querySelector(".care-block").onmouseover = function() {mouseOver2(".care-block")};
+		document.querySelector(".care-block").onmouseout = function() {mouseOut2(".care-block")};
+
+		document.querySelector(".accessory").onmouseover = () => mouseOver(".accessory-block");
+		document.querySelector(".accessory").onmouseout = function() {mouseOut(".accessory-block")};
+		document.querySelector(".accessory-block").onmouseover = function() {mouseOver2(".accessory-block")};
+		document.querySelector(".accessory-block").onmouseout = function() {mouseOut2(".accessory-block")};
+
+		document.querySelector(".dla-niej-trendy").onmouseover = () => mouseOver(".dla-niej-trendy-block");
+		document.querySelector(".dla-niej-trendy").onmouseout = function() {mouseOut(".dla-niej-trendy-block")};
+		document.querySelector(".dla-niej-trendy-block").onmouseover = function() {mouseOver2(".dla-niej-trendy-block")};
+		document.querySelector(".dla-niej-trendy-block").onmouseout = function() {mouseOut2(".dla-niej-trendy-block")};
+
+		document.querySelector(".dla-niego-trendy").onmouseover = () => mouseOver(".dla-niego-trendy-block");
+		document.querySelector(".dla-niego-trendy").onmouseout = function() {mouseOut(".dla-niego-trendy-block")};
+		document.querySelector(".dla-niego-trendy-block").onmouseover = function() {mouseOver2(".dla-niego-trendy-block")};
+		document.querySelector(".dla-niego-trendy-block").onmouseout = function() {mouseOut2(".dla-niego-trendy-block")};
+
+		document.querySelector(".dla-dziecka-trendy").onmouseover = () => mouseOver(".dla-dziecka-trendy-block");
+		document.querySelector(".dla-dziecka-trendy").onmouseout = function() {mouseOut(".dla-dziecka-trendy-block")};
+		document.querySelector(".dla-dziecka-trendy-block").onmouseover = function() {mouseOver2(".dla-dziecka-trendy-block")};
+		document.querySelector(".dla-dziecka-trendy-block").onmouseout = function() {mouseOut2(".dla-dziecka-trendy-block")};
+
+		function mouseOver(className) {
+			document.querySelector(className).style.display = "flex";
+		}
+		function mouseOut(className) {
+			document.querySelector(className).style.display = "none";
+		}
+		function mouseOver2(blockClassName) {
+			document.querySelector(blockClassName).style.display = "flex";
+		}
+		function mouseOut2(blockClassName) {
+			document.querySelector(blockClassName).style.display = "none";
+        }
+        
+//kod do alternatywnego menu
+
+    const menuBtn = document.querySelector('.burger-btn');
+    let menuOpen = false;
+    menuBtn.addEventListener('click', () => {
+        if(!menuOpen){
+            menuBtn.classList.add('open');
+            menuOpen = true;
+        } else {
+            menuBtn.classList.remove('open');
+            menuOpen = false;
+        }
+    });
