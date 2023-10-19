828eb03924b38c92a9739ca3f7a51c56108c6ddf
Grzegorz Bąk <48298051+altGreG@users.noreply.github.com>
2023-10-19 20:28:13 +0200

    Revert "dodanie plików projektu strony biblioteki"

---
+++ Anonymous/script.js
@@ -0,0 +1,4 @@
+function displayMenu(){
+    var menu = document.querySelector(".site-menu");
+    menu.classList.toggle("menu-active");
+}
--- baza-biblioteka/js/logowanie.js
+++
@@ -1,8 +0,0 @@
-//funkcja umożliwia sprawdzenie hasła, w sensie zmienia kropki w input number na zwyczajne litery i na odwrót
-function sprawdzenieHasla() {
-    var x = document.getElementById("haslo");
-    if (x.type === "password") {
-     x.type = "text";
-    } else {
-    x.type = "password";
-    }}
